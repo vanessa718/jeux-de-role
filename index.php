a571ff6898435291c1d8b27435ff69530b59e644
vanessa <vanessa.chaillou@orange.fr>
2018-03-14 10:50:27 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>jeux de rôle</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+
+
+<!--va aller chercher le contenu header dans le fichier header.php-->	
+	<?php include("header.php");?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- va chercher le contenu dans le fichier footer.php-->	
+	<?php include("footer.php");?>
+
+
+	<script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
+	<script type="text/javascript" src="js/app.js"></script>
+</body>
+</html>
